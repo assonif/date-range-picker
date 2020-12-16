31cc79e4b82f043eaa869971ee0df1e25cf9db51
Fabio Furtado <fabioassoni06@gmail.com>
2020-12-16 17:34:04 -0300

    new

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import { DateRangePicker } from "react-dates";
 import "react-dates/lib/css/_datepicker.css";
 
 import ExpandIcon from "./expand-more.svg";
+import CloseIcon from "./close-icon.svg";
 
 import "./test.css";
 
@@ -39,6 +40,7 @@ class App extends Component {
     this.state = {
       startDate: null,
       endDate: null,
+      focusedInput: null,
     };
   }
 
@@ -52,28 +54,53 @@ class App extends Component {
 
   render() {
     return (
-      <div className="container">
-        <p>Validity:</p>
+      <div
+        className="container"
+        onClick={() => console.log(this.state.startDate, this.state.endDate)}
+      >
+        <div className="last">
+          {this.state.focusedInput && (
+            <>
+              <div className="info-container">
+                <div>
+                  <p>Validity</p>
 
-        <DateRangePicker
-          startDate={this.state.startDate} // momentPropTypes.momentObj or null,
-          startDateId="your_unique_start_date_id" // PropTypes.string.isRequired,
-          endDate={this.state.endDate} // momentPropTypes.momentObj or null,
-          endDateId="your_unique_end_date_id" // PropTypes.string.isRequired,
-          onDatesChange={({ startDate, endDate }) =>
-            this.setState({ startDate, endDate })
-          } // PropTypes.func.isRequired,
-          focusedInput={this.state.focusedInput} // PropTypes.oneOf([START_DATE, END_DATE]) or null,
-          onFocusChange={(focusedInput) => this.setState({ focusedInput })} // PropTypes.func.isRequired,
-          startDatePlaceholderText="Data Inicial"
-          endDatePlaceholderText="Data Final"
-          numberOfMonths={1}
-          hideKeyboardShortcutsPanel
-          noBorder
-          reopenPickerOnClearDates
-          navPrev={this.renderIconPrev()}
-          navNext={this.renderIconNext()}
-        />
+                  <strong>
+                    {this.state.startDate && (
+                      <>
+                        {this.state.startDate.format("MMM D, YYYY")}
+                        <img className="arrow-next" src={ExpandIcon} alt="" />
+                      </>
+                    )}
+                    {this.state.endDate &&
+                      this.state.endDate.format("MMM D, YYYY")}
+                  </strong>
+                </div>
+              </div>
+              <img className="close" src={CloseIcon} alt="" />
+            </>
+          )}
+          <DateRangePicker
+            startDate={this.state.startDate} // momentPropTypes.momentObj or null,
+            startDateId="your_unique_start_date_id" // PropTypes.string.isRequired,
+            endDate={this.state.endDate} // momentPropTypes.momentObj or null,
+            endDateId="your_unique_end_date_id" // PropTypes.string.isRequired,
+            onDatesChange={({ startDate, endDate }) =>
+              this.setState({ startDate, endDate })
+            } // PropTypes.func.isRequired,
+            focusedInput={this.state.focusedInput} // PropTypes.oneOf([START_DATE, END_DATE]) or null,
+            onFocusChange={(focusedInput) => this.setState({ focusedInput })} // PropTypes.func.isRequired,
+            startDatePlaceholderText="Data Inicial"
+            endDatePlaceholderText="Data Final"
+            numberOfMonths={1}
+            hideKeyboardShortcutsPanel
+            noBorder
+            reopenPickerOnClearDates
+            navPrev={this.renderIconPrev()}
+            navNext={this.renderIconNext()}
+            keepOpenOnDateSelect
+          />
+        </div>
       </div>
     );
   }
